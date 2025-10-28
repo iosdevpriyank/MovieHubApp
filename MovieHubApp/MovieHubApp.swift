664af3e03c586182b2f0d2b09b0b18b1b1c35895
Priyank Gandhi <iosdev.priyank@gmail.com>
2025-10-28 22:30:11 +0530

    feat(initial): Initial commit of project
    Initial commit of project

---
+++ MovieHubApp/MovieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubAppApp.swift
+//  MovieHubApp
+//
+//  Created by Akshat Gandhi on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
